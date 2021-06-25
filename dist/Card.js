640b88700071eb6f4c2d7d714f51a48669cbdc90
Andrea Marchetti <andrea.marchetti@scavolini.com>
2021-06-25 16:55:01 +0200

    test npm 2

---
+++ dist/Card.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Card = props => {
+  return /*#__PURE__*/React.createElement("div", {
+    className: "card"
+  }, /*#__PURE__*/React.createElement("h1", null, "Prova"));
+};
+
+export default Card;
--- src/components/ReactElements.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-function ReactElements() {
-    return <h1>ReactElements</h1>
-}
-
-export default ReactElements;
